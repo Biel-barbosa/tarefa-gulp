8fedddfdcdb822f5532adf88e6f0956fc8abb2d0
gabriel <tlggabriel11@gmail.com>
2024-11-02 17:55:16 -0300

    adicionando default e gulp.series

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const uglify = require('gulp-uglify')
+const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 
 function comprimeImgs() {
     return gulp.src('./source/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./build/images'))
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'));
 }
 
 function comprimeJs() {
     return gulp.src('./source/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/scripts'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'));
 }
 
 function compilaSass() {
     return gulp.src('./source/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./build/styles'));
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./build/styles'));
 }
-
 
 function dizOi(callback) {
     console.log('Oi!');
-    callback(); 
+    callback();
 }
 
-exports.dizOi = dizOi;
-exports.sass = compilaSass;
-exports.javascript = comprimeJs
-exports.images = comprimeImgs
+
+const build = gulp.series(dizOi, gulp.parallel(compilaSass, comprimeJs, comprimeImgs));
+
+
+exports.default = build;
